4cc0b573ef10a58bb10d9cdab84825c41f2da7e2
Daniel <daniel.js.campbell@gmail.com>
2020-11-28 10:17:58 -0700

    'first'

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import re_path
+from basic_app import views
+
+# for template tagging, set to app name
+app_name = 'basic_app'
+
+urlpatterns = [
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^relative/$', views.relative, name='relative'),
+    re_path(r'^other/$', views.other, name='other'),
+]
